28910727ed8c57a31fe22b13d81cf01e87fe2a50
José Rojel <peperojel@gmail.com>
2021-08-02 02:51:44 -0400

    Adds submit logic to ExpenseForm & two way state binding

--- 01-starting-setup/src/components/NewExpense/ExpenseForm.js
+++ 01-starting-setup/src/components/NewExpense/ExpenseForm.js
@@ -54,12 +54,32 @@ const ExpenseForm = (props) => {
         // });
     };
 
+    const submitHandler = (event) => {
+        event.preventDefault();
+
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate),
+        };
+
+        console.log(expenseData);
+
+        setEnteredTitle("");
+        setEnteredAmount("");
+        setEnteredDate("");
+    };
+
     return (
-        <form>
+        <form onSubmit={submitHandler}>
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label>Title</label>
-                    <input type="text" onChange={titleChangeHandler} />
+                    <input
+                        type="text"
+                        onChange={titleChangeHandler}
+                        value={enteredTitle}
+                    />
                 </div>
                 <div className="new-expense__control">
                     <label>Amount</label>
@@ -68,6 +88,7 @@ const ExpenseForm = (props) => {
                         onChange={amountChangeHandler}
                         min="0.01"
                         step="0.01"
+                        value={enteredAmount}
                     />
                 </div>
                 <div className="new-expense__control">
@@ -77,6 +98,7 @@ const ExpenseForm = (props) => {
                         onChange={dateChangeHandler}
                         min="2019-01-01"
                         max="2022-12-31"
+                        value={enteredDate}
                     />
                 </div>
             </div>

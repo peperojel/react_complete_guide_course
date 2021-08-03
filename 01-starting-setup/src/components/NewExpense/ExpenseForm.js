ebd7a642cf4810a5eb9e7cf434b8c92dc165ccfd
José Rojel <peperojel@gmail.com>
2021-08-03 01:45:19 -0400

    Lifting the state up from ExpenseForm to App

--- 01-starting-setup/src/components/NewExpense/ExpenseForm.js
+++ 01-starting-setup/src/components/NewExpense/ExpenseForm.js
@@ -63,7 +63,7 @@ const ExpenseForm = (props) => {
             date: new Date(enteredDate),
         };
 
-        console.log(expenseData);
+        props.onSaveExpenseData(expenseData);
 
         setEnteredTitle("");
         setEnteredAmount("");

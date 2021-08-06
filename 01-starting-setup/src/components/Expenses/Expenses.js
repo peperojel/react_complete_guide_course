c2eba3b07b181d9363c698420ef586af9c156b1b
José Rojel <peperojel@gmail.com>
2021-08-06 01:58:36 -0400

    Rendering lists and using it as states

--- 01-starting-setup/src/components/Expenses/Expenses.js
+++ 01-starting-setup/src/components/Expenses/Expenses.js
@@ -5,18 +5,7 @@ import ExpensesFilter from "./ExpensesFilter";
 import "./Expenses.css";
 
 const Expenses = (props) => {
-    const items = [];
     const [yearFilter, setYearFilter] = useState("2020");
-    for (const expense of props.expenses) {
-        items.push(
-            <ExpenseItem
-                key={expense.id}
-                date={expense.date}
-                amount={expense.amount}
-                title={expense.title}
-            />
-        );
-    }
 
     const expenseFilteringHandler = (newFilter) => {
         console.log("Setting new year filter:", newFilter);
@@ -29,7 +18,16 @@ const Expenses = (props) => {
                 onExpensesFiltering={expenseFilteringHandler}
                 selectedYear={yearFilter}
             />
-            <Card className="expenses">{items}</Card>
+            <Card className="expenses">
+                {props.expenses.map((expense, i) => (
+                    <ExpenseItem
+                        key={i}
+                        amount={expense.amount}
+                        title={expense.title}
+                        date={expense.date}
+                    />
+                ))}
+            </Card>
         </div>
     );
 };
